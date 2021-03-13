6e1b39b47b617f6bad3485f7ffc675f3a8462a43
Aghits <aghits.assiddiq@gmail.com>
2021-03-13 18:01:40 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'custom': ['Open Sans']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
